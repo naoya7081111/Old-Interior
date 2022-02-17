9eff846ca1136322f32cad301e2bf3f4803c0dcf
naoya7081111 <naoya7081111@gmail.com>
2022-02-17 14:43:53 +0900

    スライドショーのクラス化

--- assets/js/libs/slide-show.js
+++ assets/js/libs/slide-show.js
@@ -1,28 +1,46 @@
-// class SlideShow {
-//     constructor(el) {
-//         this.el = el;
-//         this.DOM = {};
-//     }
-// }
+class HeroSlider {
+    constructor() {
+        this.swiper = this._initSwiper();
+    }
 
-const swiper = new Swiper('.swiper', {
-    // Optional parameters
-    direction: 'horizontal',
-    loop: true,
-  
-    // If we need pagination
-    pagination: {
-      el: '.swiper-pagination',
-    },
-  
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-  
-    // And if we need scrollbar
-    scrollbar: {
-      el: '.swiper-scrollbar',
-    },
-  });
+    _initSwiper() {
+        return new Swiper('.swiper', {
+            // Optional parameters
+            direction: 'horizontal',
+            loop: true,
+          
+            // If we need pagination
+            pagination: {
+              el: '.swiper-pagination',
+            },
+          
+            // Navigation arrows
+            navigation: {
+              nextEl: '.swiper-button-next',
+              prevEl: '.swiper-button-prev',
+            },
+          
+            // And if we need scrollbar
+            scrollbar: {
+              el: '.swiper-scrollbar',
+            },
+
+            speed: 1000,
+            grabCursor: true,
+        });
+    }
+
+    start(options = {}) {
+        options = Object.assign({
+            delay: 4000,
+            disableInteraction: false
+        }, options);
+        this.swiper.params.autoplay = options //paramsはスワイパークラスの2個目の引数
+        this.swiper.autoplay.start(); //スワイパークラスのautoplayに元からあるメソッドの呼び出し
+    }
+
+    stop() {
+        this.swiper.autoplay.stop();
+    }
+
+}
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,9 @@
 document.addEventListener('DOMContentLoaded', function() {
+    // テキストアニメーション
     const ta = new TweenTextAnimation('.animate-title');
     ta.animate();
+
+    // スライドショー
+    const hero = new HeroSlider();
+    hero.start({delay: 3000});
 })
